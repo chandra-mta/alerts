0cc6e273c101eb2a1ad67b2375a17dd3bbe0ca36
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2025-10-30 17:46:48 -0400

    pcademode trigger function.

--- alerts/satalerts/trigger_satalerts.py
+++ alerts/satalerts/trigger_satalerts.py
@@ -1,5 +1,11 @@
 """
 Functions for determining alert triggers
+
+:NOTE: Alert functions are custom algorithms as needed for determining if an alert should trigger,
+    or what the notification message should be. As such, they are scoped outside of an Alert class instance.
+    This means that all Alert instance attributes of importance in teh check must be passed to
+    the function as a keyword argument.
+
 """
 
 TRIGGER_SEQUENCE = (
@@ -7,8 +13,19 @@ TRIGGER_SEQUENCE = (
     fmt,
 ) #: Tuple of Function handles to run for checking all the satalerts.
 
-def pcadmode(sat_dataset):
-    pass
+def pcadmode(data):
+    """
+    Include Comm Information?
+    """
+    message = f"Chandra realtime telemetry shows PCADMODE {data.get('value')} at {data.get('cxotime')}\n"
+    if data.get('value') == "NSUN":
+        is_triggered = True
+    else:
+        is_triggered = False
+    return is_triggered, message
 
 def fmt(sat_dataset):
+    pass
+
+def run_all_satalerts():
     pass
